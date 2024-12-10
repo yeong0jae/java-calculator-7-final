200e5e7c613b3864d44d5cf7f2db403e7af14737
Yeongjae Kim <sees77770@gmail.com>
2024-12-10 15:04:07 +0900

    feat: 계산기 숫자 양수 검증 기능, 테스트 추가

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -7,7 +7,16 @@ public class Calculator {
     private final List<Integer> operands;
 
     public Calculator(List<Integer> operands) {
+        validatePositive(operands);
         this.operands = operands;
+    }
+
+    private void validatePositive(List<Integer> operands) {
+        operands.forEach(operand -> {
+            if (operand <= 0) {
+                throw new IllegalArgumentException("0 이하의 수는 입력할 수 없습니다.");
+            }
+        });
     }
 
     public int sum() {
--- src/test/java/calculator/domain/CalculatorTest.java
+++ src/test/java/calculator/domain/CalculatorTest.java
@@ -1,9 +1,12 @@
 package calculator.domain;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CalculatorTest {
 
@@ -16,5 +19,20 @@ class CalculatorTest {
         int sum = calculator.sum();
 
         Assertions.assertThat(sum).isEqualTo(6);
+    }
+
+    @DisplayName("계산기의 숫자가 양수가 아니라면 예외 처리한다.")
+    @ParameterizedTest
+    @MethodSource("provideNumberList")
+    void validatePositiveTest(List<Integer> numbers) {
+        Assertions.assertThatThrownBy(() -> new Calculator(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<List<Integer>> provideNumberList() {
+        return Stream.of(
+                List.of(-1, 2, 3),
+                List.of(0, 2, 3)
+        );
     }
 }
